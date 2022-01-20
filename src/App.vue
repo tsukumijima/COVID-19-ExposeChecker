a7880ca31ba6a96dd6f493f912f539e2497c2595
tsukumi <tsukumijima@users.noreply.github.com>
2022-01-21 02:39:57 +0900

    「ホーム画面に追加」ボタンを押した際、可能であればインストールプロンプトを表示するように変更

--- src/App.vue
+++ src/App.vue
@@ -11,12 +11,13 @@
       <v-spacer></v-spacer>
       <div class="d-flex align-center">
         <v-btn
-          @click="dialog=true"
           text
           small
+          class="px-3 py-5"
+          @click="installPromptEvent !== null ? installPromptEvent.prompt() : dialog = true"
         >
           <v-icon>mdi-plus-box-outline</v-icon>
-          <span>ホーム画面に追加</span>
+          <span class="ml-1">ホーム画面に追加</span>
         </v-btn>
       </div>
       <!-- <v-btn
@@ -36,8 +37,8 @@
       <v-card>
         <v-card-title class="text-h5 lighten-2">ホーム画面に追加</v-card-title>
         <v-card-text>
-          ブラウザの下記ボタンを押して<b>ホーム画面に追加</b>を押すと、ホーム画面上にショートカットを作成できます。<br>
-          ショートカットを作成し、定期的にログを解析することをおすすめします。<br><br>
+          ブラウザの下記ボタンを押して <b>ホーム画面に追加</b> を押すと、ホーム画面上にアイコンを作成できます。<br>
+          ホーム画面のアイコンから、定期的にログを解析することをおすすめします。<br><br>
           <v-row>
             <v-col cols=6>
               <v-img src="@/assets/images/add-home-ios.png"/>
@@ -46,8 +47,8 @@
               <v-img src="@/assets/images/add-home-android.png"/>
             </v-col>
           </v-row>
-          <p class="text-center">
-            (左:iOS / 右:Android)<br><br>
+          <p class="text-center mt-2">
+            (左:iOS / 右:Android)
           </p>
           <v-img class="mx-8" src="@/assets/images/home-icon.png"/>
         </v-card-text>
@@ -72,15 +73,28 @@
 </template>
 
 <script lang="ts">
+import Vue from 'vue';
 import ExposeChecker from './components/ExposeChecker.vue';
 
-export default {
+export default Vue.extend({
   name: 'App',
   components: {
     ExposeChecker,
   },
-  data: () => ({
-    dialog: false,
-  }),
-};
+  data() {
+    return {
+      // ダイヤログの v-model
+      dialog: false,
+      // PWA のインストールプロンプトを表示するためのイベント
+      installPromptEvent: null,
+    }
+  },
+  created() {
+    // PWA のインストールプロンプトを表示するためのイベントを保存する
+    // ref: https://qiita.com/kan_dai/items/359c4c138dcbb94e9bb5
+    window.addEventListener('beforeinstallprompt', (event) => {
+      this.installPromptEvent = event;
+    });
+  }
+});
 </script>
